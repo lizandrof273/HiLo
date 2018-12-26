93237c3de47e8e7b7878fb8c0cb963136714d204
Lizandro Franco <lizandrofranco@lizandros-mbp.attlocal.net>
2018-12-26 15:27:47 -0800

    random number generated

---
+++ src/HiLo.java
@@ -0,0 +1,11 @@
+
+public class HiLo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Create a number for user to guess
+		int theNumber = (int)(Math.random()*100 +1);
+		System.out.println( theNumber );
+	}
+
+}
