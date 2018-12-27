2d78f71f90f4a674206d7bf45a4829619b39f737
Lizandro Franco <lizandrofranco@lizandros-mbp.attlocal.net>
2018-12-26 16:41:10 -0800

    got users guess

--- src/hiLow/HilLo.java
+++ src/hiLow/HilLo.java
@@ -1,13 +1,19 @@
 package hiLow;
-
+import java.util.Scanner;
 public class HilLo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Create a number for user to guess
-		int theNumber = (int)(Math.random()*100 +1);
+		Scanner scan = new Scanner(System.in);
+		int theNumber = (int)(Math.random()*1000 +1);
 		System.out.println( theNumber );
+		int guess = 0;
+		System.out.println("Guess a number between 1 and 100:");
+		guess = scan.nextInt();
+		
 	}
 
 }
+
 
