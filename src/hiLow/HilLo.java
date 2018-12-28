1118feeab202d2edf53bab216076977d63f56622
Lizandro Franco <lizandrofranco@lizandros-mbp.attlocal.net>
2018-12-28 15:37:19 -0800

    output users guess

--- src/hiLow/HilLo.java
+++ src/hiLow/HilLo.java
@@ -7,10 +7,11 @@ public class HilLo {
 		// Create a number for user to guess
 		Scanner scan = new Scanner(System.in);
 		int theNumber = (int)(Math.random()*1000 +1);
-		System.out.println( theNumber );
+		// System.out.println( theNumber );
 		int guess = 0;
 		System.out.println("Guess a number between 1 and 100:");
 		guess = scan.nextInt();
+		System.out.println( "You entered " + guess + "." );
 		
 	}
 
